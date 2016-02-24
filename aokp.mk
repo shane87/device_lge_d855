ecad9c493703d781cf58d1be8570938f4a6052b9
Adam <ovadia.adam@gmail.com>
2016-02-24 11:38:49 -0500

    aokp-ify

--- aokp.mk
+++ aokp.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/d855/full_d855.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d855
+PRODUCT_NAME := aokp_d855
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
