LineageOS/android_device_lge_d855

d855: Update NFC hal name
Change-Id: Ic783fd83a68cd3a9460656d7e36729da23dfff64

device.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
-    nfc_nci.pn54x.default
+    nfc_nci.d855
 
 # FM Radio
 PRODUCT_PACKAGES += \

shane87/device_lge_d855

device.mk
@@ -57,7 +57,7 @@ PRODUCT_PACKAGES += \
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
-    nfc_nci.pn54x.default
+    nfc_nci.msm8974
 
 # FM Radio
 PRODUCT_PACKAGES += \